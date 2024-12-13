3ba6eb91a485fc557a092758e0e2c84d5f10150f
César García <cesar.garciamcs@outlook.com>
2024-12-12 21:54:50 -0600

    eliminando producto por talla y id

--- js/app.js
+++ js/app.js
@@ -189,11 +189,11 @@ function agregarCurso(e) {
 }
 
 // Elimina un curso del carrito
-function eliminarCurso(id) {
-    console.log(articulosCarrito.filter( curso => curso.id !== id ));
+function eliminarCurso(id, talla) {
+    console.log(articulosCarrito.filter( curso => !(curso.id === Number(id) && curso.talla === talla) ));
 
     // Elimina del arreglo articulosCarrito por el data-id
-    articulosCarrito = articulosCarrito.filter( curso => curso.id !== Number(id) );
+    articulosCarrito = articulosCarrito.filter( curso => !(curso.id === Number(id) && curso.talla === talla) );
 
     carritoHTML(); // Iterar sobre el carrito y mostrar su HTML
 }
@@ -203,7 +203,9 @@ function manejarClick(e) {
     console.log(e.target.closest('tr').querySelector('.borrar-curso').dataset.id);
     if(e.target.closest('.borrar-curso')) {
         const cursoId = e.target.closest('.borrar-curso').dataset.id;
-        eliminarCurso(cursoId);
+        const talla = e.target.closest('tr').querySelector('.talla-producto').textContent.trim();
+
+        eliminarCurso(cursoId, talla);
         
     } else if (e.target.closest('.carrito-storage')) { // Asegurarse de que se hizo click en el botón más o menos
         const accion = e.target.closest('.carrito-storage').id; // 'mas' o 'menos'
@@ -260,7 +262,7 @@ function carritoHTML() {
                     </a>
                 </div>
             </td>
-            <td> ${talla} </td>
+            <td class="talla-producto"> ${talla} </td>
             <td>
                 <a href="#" class="borrar-curso" data-id="${id}">
                     <svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 24 24" fill="rgba(247,58,58,1)"><path d="M4 8H20V21C20 21.5523 19.5523 22 19 22H5C4.44772 22 4 21.5523 4 21V8ZM7 5V3C7 2.44772 7.44772 2 8 2H16C16.5523 2 17 2.44772 17 3V5H22V7H2V5H7ZM9 4V5H15V4H9ZM9 12V18H11V12H9ZM13 12V18H15V12H13Z"></path></svg>
@@ -295,22 +297,20 @@ function leerDatosCurso(curso, talla, cantidad) {
         imagen: producto.imagen,
         titulo: producto.nombre,
         id: producto.id,
-        cantidad: cantidad,
+        cantidad: Number(cantidad),
         talla: talla
     };
 
     // Revisa si un elemento ya existe en el carrito
-    const existe = articulosCarrito.some(curso => curso.id === infoCurso.id);
+    const existe = articulosCarrito.some(curso => curso.id === infoCurso.id && curso.talla === infoCurso.talla);
     if (existe) {
         // Actualizamos la cantidad y talla
         const cursos = articulosCarrito.map(curso => {
-            if (curso.id === infoCurso.id) {
-                curso.cantidad = cantidad;
-                curso.talla = talla;
-                return curso; // Retorna el objeto actualizado
-            } else {
-                return curso; // Retorna los objetos que no son duplicados
+            if (curso.id === infoCurso.id && curso.talla === infoCurso.talla) {
+                curso.cantidad += infoCurso.cantidad;
             }
+            
+            return curso;
         });
         articulosCarrito = [...cursos];
     } else {
