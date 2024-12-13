7eef234cb63b8c01fed936c22a4f60bb48be2017
César García <cesar.garciamcs@outlook.com>
2024-12-12 20:59:56 -0600

    cambiando a un svg para el boton borrar

--- js/app.js
+++ js/app.js
@@ -187,8 +187,8 @@ function agregarCurso(e) {
 
 // Elimina un curso del carrito
 function eliminarCurso(e) {
-    if(e.target.classList.contains('borrar-curso')) {
-        const cursoId = e.target.getAttribute('data-id');
+    if(e.target.closest('.borrar-curso')) {
+        const cursoId = e.target.closest('.borrar-curso').dataset.id;
 
         console.log(articulosCarrito.filter( curso => curso.id !== cursoId ));
 
@@ -268,10 +268,10 @@ function manejarClick(e) {
 
 // Modifica la cantidad en el carrito
 function modificarCantidad(id, delta) {
-    console.log(id, delta);
+    console.log(delta);
     const cantidad = articulosCarrito.map(curso => {
         if (curso.id === Number(id)) {
-            curso.cantidad = Math.max(0, curso.cantidad + Number(delta)); // Evita cantidades negativas
+            curso.cantidad = Math.max(0, Number(curso.cantidad) + delta); // Evita cantidades negativas
         }
         console.log(curso);
         return curso;
@@ -309,7 +309,9 @@ function carritoHTML() {
             </td>
             <td> ${talla} </td>
             <td>
-                <a href="#" class="borrar-curso" data-id="${id}">X</a>
+                <a href="#" class="borrar-curso" data-id="${id}">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 24 24" fill="rgba(247,58,58,1)"><path d="M4 8H20V21C20 21.5523 19.5523 22 19 22H5C4.44772 22 4 21.5523 4 21V8ZM7 5V3C7 2.44772 7.44772 2 8 2H16C16.5523 2 17 2.44772 17 3V5H22V7H2V5H7ZM9 4V5H15V4H9ZM9 12V18H11V12H9ZM13 12V18H15V12H13Z"></path></svg>
+                </a>
             </td>
         `;
 
